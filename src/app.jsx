87a9306ca7dbbe3e4894061ca21777a94e1a4da5
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-02-27 20:10:45 +0545

    create react component and use es6 module

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+// Separation of Concern
+
+import Contact from './contact'
+
+function App() {
+  return (
+    <div>
+      <Contact />
+    </div>
+  )
+}
+
+// default export
+export default App
