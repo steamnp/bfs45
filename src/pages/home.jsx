0d3babdd4aad6a8cd656212e53a4d0970a8ac130
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-02-29 13:50:47 -0600

    assignment

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,23 +1,27 @@
-import Button from '../component/button'
-import Store from './store'
+import Button from "../component/button";
+// import Store from './store'
 
 function Home() {
   return (
     <div>
-      <Button>123456</Button>
-      <Button type="view">123456</Button>
+      <Button>Button</Button>
+      <Button type="view">View</Button>
+      <Button type="view--all">View All</Button>
+      <Button type="remove">Remove</Button>
+      <Button type="back">Back</Button>
+      <Button type="get-started">Get Started</Button>
       {/* <Button type="view-all">New Button</Button>
       <Button type="back" /> */}
       {/* <Store /> */}
     </div>
-  )
+  );
 }
 
 // <div className="sth" id="one"></div> -> className,id are attribute
 
 //<Button buttonText={} /> -> buttonText -> prop
 
-export default Home
+export default Home;
 
 {
   /* <button>Home button</button> */
