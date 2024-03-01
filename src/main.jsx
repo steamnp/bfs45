a705920046b20fa2eb93699cd1fbf2646a04ae8a
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-03-01 21:07:57 +0545

    use react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/home'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import Store from './pages/store'
+import Contact from './pages/contact'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/store',
+    element: <Store />,
+  },
+  {
+    path: '/contact',
+    element: <Contact />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
