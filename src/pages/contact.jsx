9b065ad177e4cf732132cb2160537023e5e3ed0a
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-03-01 21:21:43 +0545

    add task for js

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,15 +1,4 @@
-// Global Scope
-
-const firstNumber = 20
-
-if (firstNumber > 20) {
-  // Block Scope
-}
-
 function Contact() {
-  // Function Scope
-  // can wirte js logic here
-
   return <div>Contact</div>
 }
 
