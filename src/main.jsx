23cb08839e06534e38703b73677f7f3f6b08eeda
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-01 09:26:07 -0600

    implement react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './pages/home'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/home";
+import Store from "./pages/store";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/store",
+    element: <Store />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
