40ebfc8775cbc09b674bb10dff23d183b4ac407b
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-03-01 21:16:03 +0545

    scope and js location

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,15 @@
+// Global Scope
+
+const firstNumber = 20
+
+if (firstNumber > 20) {
+  // Block Scope
+}
+
 function Contact() {
+  // Function Scope
+  // can wirte js logic here
+
   return <div>Contact</div>
 }
 
