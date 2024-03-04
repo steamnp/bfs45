647e327aa84ddc2cd02a2a6e3b2733ed7d244304
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-03-04 22:50:12 +0545

    access array elemment

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+function App() {
+  const primeNumbers = [2, 3, 5, 7]
+
+  console.log(primeNumbers[2])
+
+  return <div>App</div>
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/routes'
-import './index.css'
+import App from './app'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <RouterProvider router={router} />
+  <App />
   // </React.StrictMode>
 )
