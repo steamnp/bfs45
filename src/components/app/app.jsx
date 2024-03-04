1ef09fc33b55ebfd40d80b27094077a04b7578e9
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-04 14:08:48 -0600

    check prime number

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -12,10 +12,26 @@ function App() {
     return sum;
   };
 
+  const isPrime = (num) => {
+    if (num < 2) return false;
+
+    let i = 2;
+
+    while (i < num - 1) {
+      if (num % i === 0) return false;
+      i++;
+    }
+
+    return true;
+  };
+
   return (
     <>
       <div style={{ padding: "1rem" }}>
         The sum of odd numbers: {oddSum([1, 2, 3, 4, 5, 6, 7, 8, 9])}
+        <br />
+        <br />
+        Is it prime number?: {JSON.stringify(isPrime(8))}
       </div>
     </>
   );
