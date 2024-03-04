6f41816bb2a5d31959833bbcbd437708178a4329
sonamLama22 <lamadsonam19@gmail.com>
2024-03-04 15:34:42 -0500

    JS assessment

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,39 @@
 function App() {
-  // function in javascript
-  // Function declaration -> Hoisting issue
-  // console.log(add(2, 3))
-  // function add(firstNumber, secondNuber) {
-  //   return firstNumber + secondNuber
-  // }
+  // write a function which will add all the odd numbers and give sum of odd numbers.
+  const arr = [1, 2, 3, 4, 5, 6, 7];
+  let sum = 0;
+  arr.map((item) => {
+    if (item % 2 !== 0) {
+      sum += item;
+    }
+  });
+  console.log(sum);
 
-  // Function Expression -> ES6 -> Not hoisted
-  // const add = function (firstNumber, secondNuber) {
-  //   return firstNumber + secondNuber
-  // }
+  // prime number. return true if prime, false otherwise.
+  let flag = false;
+  function isPrime(num) {
+    if (num <= 1) {
+      return flag;
+    }
 
-  // Arrow Function -> ES6 -> Not hoisted
-  const add = (firstNumber, secondNumber) => firstNumber + secondNumber
-
-  // console.log(myCountry)
-  // var myCountry = 'Nepal' // var is also hoisted
-
-  const firstName = 'Gorakh'
-  const lastName = 'Joshi'
-
-  if (firstName) {
-    const fullName = `${firstName} ${lastName}`
+    for (let i = 2; i < num / 2; i++) {
+      // O(sqrt(n))
+      if (num % i === 0) {
+        return flag;
+      }
+    }
+    return true;
   }
 
-  return <h1>{add(1.2, 3.2)}</h1>
+  const result = isPrime(6) === true ? "true" : "false";
+  console.log(result);
+
+  return (
+    <div>
+      <h1>{sum}</h1>
+      <h1>{result}</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
