b675741d461dba4d6b37854f24c2e726ecf1895d
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-03-04 23:01:33 +0545

    object in javascript

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,18 @@
 function App() {
-  const primeNumbers = [2, 3, 5, 7]
+  const personArr = ['Alex', 25]
 
-  console.log(primeNumbers[2])
+  // console.log(personArr[1])
+
+  const person = {
+    name: {
+      firstName: 'Gorakh',
+      lastName: 'Joshi',
+    },
+    age: 25,
+    '#SN': 1,
+  }
+
+  console.log(person.name.firstName)
 
   return <div>App</div>
 }
