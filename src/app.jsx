fdb8eb374fbba0f9ab9baa864bc268976b05697d
Sajan Gurung <gsajan21@gmail.com>
2024-03-04 11:54:18 -0800

    classwork: add all odd numbers, and check if number is prime

--- src/app.jsx
+++ src/app.jsx
@@ -12,19 +12,58 @@ function App() {
   // }
 
   // Arrow Function -> ES6 -> Not hoisted
-  const add = (firstNumber, secondNumber) => firstNumber + secondNumber
+  // const add = (firstNumber, secondNumber) => firstNumber + secondNumber
 
   // console.log(myCountry)
   // var myCountry = 'Nepal' // var is also hoisted
 
-  const firstName = 'Gorakh'
-  const lastName = 'Joshi'
+  // const firstName = 'Gorakh'
+  // const lastName = 'Joshi'
 
-  if (firstName) {
-    const fullName = `${firstName} ${lastName}`
+  // if (firstName) {
+  //   const fullName = `${firstName} ${lastName}`
+  // }
+
+  // Find the sum of odd numbers from the list
+
+
+  // const listOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  
+  // const addOddNumbers = function (listOfNumbers){
+  //   let sum = 0;
+  //   for(let i = 0; i < listOfNumbers.length; i++){
+  //     if(listOfNumbers[i] % 2 != 0){
+  //       sum += listOfNumbers[i]
+  //     }
+  //   }
+  //   return sum
+  // }
+
+  // let returnedSum = addOddNumbers(listOfNumbers)
+  // console.log(returnedSum)
+
+
+  // check if the number is prime or not
+
+  const isPrime = 3
+
+  const checkPrimeNumber = function(isPrime){
+    if(isPrime < 1){
+      return false
+    }
+
+    for(let i= 2; i < (isPrime/2) + 1; i ++){
+      if( isPrime % i === 0){
+        return false
+      }
+    }
+    return true;
   }
 
-  return <h1>{add(1.2, 3.2)}</h1>
+  let trueOrFalse = checkPrimeNumber(isPrime)
+  console.log(trueOrFalse)
+
+  return <h1>Hello</h1>
 }
 
 export default App
