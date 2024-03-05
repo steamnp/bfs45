0b123d50026c03fdb1db93ee9e06ccd42eedfd92
sonamLama22 <lamadsonam19@gmail.com>
2024-03-04 19:01:54 -0500

    Add CSS styling

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,9 +4,12 @@ import Store from "./store";
 function Home() {
   return (
     <div>
-      <Button type="view-all">Home Button</Button>
+      <Button type="view--all">View all Button</Button>
       <Store />
-      <Button type="remove">New Button</Button>
+      <Button type="remove">Remove Button</Button>
+      <Button type="view">View Button</Button>
+      <Button type="back">Back Button</Button>
+      <Button type="get-started">Get Started Button</Button>
       <Button />
     </div>
   );
