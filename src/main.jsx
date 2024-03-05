e7807a92d7f147b2be0cda6272b26d35e8a495c7
sonamLama22 <lamadsonam19@gmail.com>
2024-03-04 20:05:19 -0500

    add CSS modules

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/home";
 import Store from "./pages/store";
+import "../index.css";
 
 const router = createBrowserRouter([
   {
