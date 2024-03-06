2c202f4b1def0127ad762613cffa6079277f82a5
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-06 13:46:00 -0600

    setup routes

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,43 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./components/Home";
+import Details from "./components/Details";
+
+const routes = [
+  {
+    to: "/",
+    text: "home",
+    state: {},
+    element: <Home />,
+  },
+  {
+    to: "/details/:name/",
+    text: "details",
+    state: {},
+    element: <Details />,
+  },
+];
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className="page">
+          <Routes>
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                exact={true}
+                path={route.to}
+                state={route.state}
+                element={route.element}
+              />
+            ))}
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
