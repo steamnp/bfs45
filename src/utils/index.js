7f3a5d7ac649e0d7598b2c3c0d95cda5dda1cc53
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-06 14:18:13 -0600

    export api functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,16 +1,14 @@
 const COUNTRY_LIST_API = "https://restcountries.com/v3.1/all";
 const COUNTRY_DETAIL_API = "https://restcountries.com/v3.1/name/"; // ADD COUNTRY NAME AT THE END
 
-async function getCountryList() {
+export async function getCountryList() {
   const countries = await fetch(COUNTRY_LIST_API);
 
   return await countries.json();
 }
 
-async function getCountryDetail(country) {
+export async function getCountryDetail(country) {
   const detail = await fetch(COUNTRY_DETAIL_API + country);
 
   return await detail.json();
 }
-
-export default { getCountryDetail, getCountryList };
