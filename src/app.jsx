71e1fa712db8c2f9dbf92c5efe61ef8f1f87278e
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-06 13:34:17 -0600

    cleanup project

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,5 @@
 function App() {
-  const oddSum = (listOfNumbers) =>
-    listOfNumbers.reduce((sum, num) => {
-      if (num % 2 !== 0) sum += num;
-      return sum;
-    });
-
-  const isPrime = (num) => {
-    if (num < 2) return false;
-
-    let i = 2;
-
-    while (i < num / 2) {
-      if (num % i === 0) return false;
-      i++;
-    }
-
-    return true;
-  };
-
-  return (
-    <>
-      <div style={{ padding: "1rem" }}>
-        The sum of odd numbers: {oddSum([1, 2, 3, 4, 5, 6, 7, 8, 9])}
-        <br />
-        <br />
-        Is it prime number?: {JSON.stringify(isPrime(8))}
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from "./app";
+import App from "./App";
 import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
