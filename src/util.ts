5fb6af23d54bf2d18bb8cc45fd702b452741b8a8
Sajan Gurung <gsajan21@gmail.com>
2024-03-07 11:15:52 -0800

    learn typescript

---
+++ src/util.ts
@@ -0,0 +1,15 @@
+
+let unknownVariable: number
+
+unknownVariable = 20
+unknownVariable = 40
+
+// unknownVariable = "Sajan" // can't do this
+
+console.log(unknownVariable)
+
+// type inference
+
+let age = 40
+age = 50
+console.log(age)
