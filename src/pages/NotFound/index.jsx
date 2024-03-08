f88bb71de46fdba0f5eb0e1fa4794a3697290c82
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-08 11:53:36 -0600

    add link to not found page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+
+import Button from "../../components/Button";
 import FlexBox from "../../components/FlexBox";
 
 import style from "./notfound.module.css";
@@ -6,8 +8,15 @@ import style from "./notfound.module.css";
 function NotFound() {
   return (
     <div>
-      <FlexBox center={true} style={{ height: "100vh" }}>
+      <FlexBox
+        center={true}
+        style={{ height: "100vh", flexDirection: "column" }}
+      >
         <div className={style.title}>Page Not Found</div>
+        <br />
+        <Button to="/" as="link">
+          Go back to home
+        </Button>
       </FlexBox>
     </div>
   );
