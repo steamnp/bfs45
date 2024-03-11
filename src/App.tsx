997436efc6dfeca308ca19ce84e1dc124461de11
mwdorji13 <mwdorji@miners.utep.edu>
2024-03-11 10:52:23 -0600

    installed redux, created redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div>App</div>
-}
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import store from './app/store'
+import { Provider } from 'react-redux'
 
-export default App
+// As of React 18
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+)
