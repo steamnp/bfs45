90fc8ad9e26c7a07b330a77b4244b6a72f32c81f
Sajan Gurung <gsajan21@gmail.com>
2024-03-11 11:14:45 -0700

    jargon

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { Provider } from 'react-redux'
-import store from './app/store.js'
+import { Provider } from "react-redux";
+import store from "./app/store.js";
 
-import App from './App.tsx'
-
+import App from "./App.tsx";
 
 // As of React 18
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-)
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
