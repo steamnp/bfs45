bfda0040a676307e24727372bcadcb7b68dcd5ea
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-03-11 21:52:18 +0545

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,5 @@
-import { createContext, useState } from 'react'
-import Home from './pages/home'
-import React from 'react'
-import { MyContext } from './context/my-context'
-
-export const menuData = ['Home', 'About', 'Store', 'Contact']
-
 function App() {
-  const [menu, setMenu] = useState(menuData)
-
-  function onUpdate(updatedData) {
-    setMenu(updatedData)
-  }
-  return (
-    <MyContext.Provider value={menu}>
-      <div className="container">
-        <div>Menu from App component</div>
-        {menu.map((menuItem, index) => {
-          return <span key={index}>{menuItem}</span>
-        })}
-        <Home />
-      </div>
-    </MyContext.Provider>
-  )
+  return <div>App</div>
 }
 
 export default App
