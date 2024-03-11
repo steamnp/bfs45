df666ff2af899f02ce50cece0558a218e8dd1685
Nima Sherpa <sherpa2025@gmail.com>
2024-03-11 10:44:21 -0600

    create redux store to react

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import store from "./app/store.tsx";
+import { Provider } from "react-redux";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
-)
+);
