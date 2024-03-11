34c5c750619eb24ab7b08912e193b0c705a8116d
bik-on-git <bikash.me.shrestha@gmail.com>
2024-03-11 09:46:38 -0700

    redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import store from "./app/store.ts";
+import { Provider } from "react-redux";
 import App from './App.tsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+
+
+    </Provider>
+
   </React.StrictMode>
 )
