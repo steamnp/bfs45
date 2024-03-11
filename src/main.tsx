1a3c52edfb403b66a292014ff36af63553fc766a
Sajan Gurung <gsajan21@gmail.com>
2024-03-11 09:39:12 -0700

    create redux store and providestore to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { Provider } from 'react-redux'
+import store from './app/store.js'
+
 import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+
+// As of React 18
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
 )
