d4b9ad779128eea42cc531fce03f70cf6d32af9e
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-11 15:25:37 -0500

    list all pokemons:

--- src/pages/Task2/index.tsx
+++ src/pages/Task2/index.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { useGetAllPokemonQuery } from "../../services/pokemon";
+import { Pokemon } from "../../types";
+
+import style from "./task.module.css";
+import { getRoute } from "../../routes";
 
 function Task2() {
-  return <div>Task2</div>;
+  const { data, error, isLoading } = useGetAllPokemonQuery("");
+
+  return (
+    <div>
+      {error ? (
+        <div className={style.info}>Oh no, there was an error</div>
+      ) : isLoading ? (
+        <div className={style.info}>Loading...</div>
+      ) : data ? (
+        <div>
+          {data.results.map((pokemon: Pokemon) => (
+            <Link
+              to={getRoute("Task 2 Detail").to.replace(":name", pokemon.name)}
+              key={pokemon.name}
+              className={style.list}
+            >
+              {pokemon.name}
+            </Link>
+          ))}
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
 export default Task2;
