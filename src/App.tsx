87d058186431dc535e9d77054bce3b5a0d73ac46
bik-on-git <bikash.me.shrestha@gmail.com>
2024-03-12 08:13:01 -0700

    counterApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { RootState } from "@reduxjs/toolkit/query";
+import { increment, decrement, incrementByAmount } from "./feature/CounterSlice"
+import { UseSelector, useDispatch, useSelector } from "react-redux";
+
 function App() {
-  return <div>App</div>
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count : {count}</p>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>Increment by 5</button>
+
+
+
+    </div>)
 }
 
 export default App
