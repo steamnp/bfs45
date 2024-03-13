bb07e2fd9d920b1ec75cfa7d9f6426757f577f65
aashirwad43 <aashirwad43@gmail.com>
2024-03-13 11:28:35 -0500

    add home page and global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import "./App.css";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/contacts" />} />
+      <Route path="/contacts" element={<HomePage />} />
+      <Route path="*" element={<HomePage />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import App from "./App";
 import "./index.css";
-ReactDOM.createRoot(document.getElementById("root")!).render(
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
