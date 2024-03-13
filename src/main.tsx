72f944df3647766af52bb89484b47d0e220a2ac8
bik-on-git <bikash.me.shrestha@gmail.com>
2024-03-13 09:56:22 -0700

    fixed issue/updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import {createRoot} from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <Router>
         <App />
-      </BrowserRouter>
+      </Router>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
