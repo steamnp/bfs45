235692b27a6efb3a863f15edb8a0709f9614715f
nirjala123 <parajulinirjala@gmail.com>
2024-03-13 10:53:49 -0500

    migrate and change to newer version

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import { store } from "./app/store";
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-)
+  document.getElementById("root")
+);
