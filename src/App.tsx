ae9e5600508aef97c5a93fbc9a0ad888837e3d66
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-13 09:36:34 -0500

    fix react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+
+import HomePage from "./Pages/HomePage";
+import "./App.css";
+
 function App() {
-  return <>Hello World</>;
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname === "/") navigate("/contacts");
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/contacts" element={<HomePage />} />
+      <Route path="*" element={<HomePage />} />
+    </Routes>
+  );
 }
 
 export default App;
