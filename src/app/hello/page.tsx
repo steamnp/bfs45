9be558143a0633a475514d7444e827ef8c90212d
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-14 11:57:11 -0500

    next js app

---
+++ src/app/hello/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hello() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      Hello,World!
+    </div>
+  );
+}
+
+export default Hello;
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      It does not do to dwell on dreams and forget to live.
+      <br />
+      <br />– Albus Dumbledore
+    </main>
+  );
+}
