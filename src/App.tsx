41cb925e3e1ec2b020fe647bcedf8e3befc22467
nirjala123 <parajulinirjala@gmail.com>
2024-03-15 10:00:31 -0500

    initial commit to setup the testing project

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { AddThoughtForm } from "./AddThoughtForm";
+import { Thought } from "./Thought";
+import { IThought } from "./types";
+import { generateId, getNewExpirationTime } from "./utils/utils";
+
+const App = () => {
+  const [thoughts, setThoughts] = useState([
+    {
+      id: generateId(),
+      text: "This is a place for your passing thoughts.",
+      expiresAt: getNewExpirationTime(),
+    },
+    {
+      id: generateId(),
+      text: "They'll be removed after 15 seconds.",
+      expiresAt: getNewExpirationTime(),
+    },
+  ]);
+
+  const addThought = (thought: IThought) => {
+    setThoughts((thoughts) => [thought, ...thoughts]);
+  };
+
+  const removeThought = (thoughtIdToRemove: number) => {
+    setThoughts((thoughts) =>
+      thoughts.filter((thought) => thought.id !== thoughtIdToRemove)
+    );
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Passing Thoughts</h1>
+      </header>
+
+      <main>
+        <AddThoughtForm addThought={addThought} />
+        <ul className="thoughts">
+          {thoughts.map((thought) => (
+            <Thought
+              removeThought={removeThought}
+              key={thought.id}
+              thought={thought}
+            />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface IThought {
+  id: number;
+  text: string;
+  expiresAt: number;
+}
